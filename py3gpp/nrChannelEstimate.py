d7afa81ce10fab2d1f604afeaa195062e41b2fe6
Benjamin Menkuec <benjamin@menkuec.de>
2022-11-26 23:30:42 +0100

    - implement linear interpolation in nrChannelEstimate - simplify nrSetResources

--- py3gpp/nrChannelEstimate.py
+++ py3gpp/nrChannelEstimate.py
@@ -10,6 +10,12 @@ def nrChannelEstimate(rxGrid = None, refInd = None, refSym = None, refGrid = Non
             return
         refGrid = np.zeros(rxGrid.shape, 'complex')
         nrSetResources(refInd, refGrid, refSym)
+    else:
+        pilot_idx = np.where(refGrid.ravel(order='F') != 0)
+        refInd = pilot_idx[0]
+        refSym = refGrid.ravel(order='F')[refInd] 
+
+    rxSym = rxGrid.ravel(order='F')[refInd]         
     L = 14 if cp == 'normal' else 12
     K = rxGrid.shape[0] # number of subcarriers
     N = rxGrid.shape[1] # number of symbols
@@ -18,15 +24,21 @@ def nrChannelEstimate(rxGrid = None, refInd = None, refSym = None, refGrid = Non
     normGrid = rxGrid/refGrid
     idx_nonzero = np.where(np.invert(np.isinf(normGrid.ravel(order='F'))))[0]
     normXYZ = np.column_stack((C.ravel(order='F')[idx_nonzero], R.ravel(order='F')[idx_nonzero], normGrid.ravel(order='F')[idx_nonzero]))
-    #interp_real = scipy.interpolate.interp2d(normXYZ[:,0].real, normXYZ[:,1].real, np.real(normXYZ[:,2]), kind='linear')
-    #interp_imag = scipy.interpolate.interp2d(normXYZ[:,0].real, normXYZ[:,1].real, np.imag(normXYZ[:,2]), kind='linear')
-    #H = interp_real(np.arange(K), np.arange(N)).reshape(refGrid.shape) + 1j*interp_imag(np.arange(K), np.arange(N)).reshape(refGrid.shape)
     
-    # use RBFInterpolator because interp2d and griddata cannot extrapolate
-    H = scipy.interpolate.griddata((normXYZ[:,0].real, normXYZ[:,1].real), 
-        normXYZ[:,2], (C.ravel(order='F'), R.ravel(order='F'))).reshape(refGrid.shape, order='F')
+    if True:
+        # this one works best!
+        for col in range(N):
+            idx = np.where(np.logical_and(refInd >= col*K, refInd < (col+1)*K))[0]
+            if len(idx) > 0:
+                sym_idx = refInd[idx] - col*K
+                H[:,col] = np.interp(np.arange(K), sym_idx, rxSym[idx]/refSym[idx])
 
-    #H = scipy.interpolate.RBFInterpolator(np.array((normXYZ[:,0].real, normXYZ[:,1].real)).T, normXYZ[:,2])
-    interpolator = scipy.interpolate.RBFInterpolator(np.array((normXYZ[:,0].real, normXYZ[:,1].real)).T, normXYZ[:,2])
-    H = interpolator(np.vstack((C.ravel(order='F'), R.ravel(order='F'))).T).reshape(refGrid.shape, order='F')
+    elif False:
+        H = scipy.interpolate.griddata((normXYZ[:,0].real, normXYZ[:,1].real), 
+            normXYZ[:,2], (C.ravel(order='F'), R.ravel(order='F'))).reshape(refGrid.shape, order='F')
+    elif False:    
+        # use RBFInterpolator because interp2d and griddata cannot extrapolate
+        interpolator = scipy.interpolate.RBFInterpolator(np.array((normXYZ[:,0].real, normXYZ[:,1].real)).T, normXYZ[:,2])
+        H = interpolator(np.vstack((C.ravel(order='F'), R.ravel(order='F'))).T).reshape(refGrid.shape, order='F')
+        
     return H
