bece88a34bb1872ba56f133799a3c7dfbb4b3dba
Benjamin Menkuec <benjamin@menkuec.de>
2023-08-05 13:49:43 +0200

    implement phase compensation for nrOFDMModulate

--- py3gpp/nrOFDMModulate.py
+++ py3gpp/nrOFDMModulate.py
@@ -4,7 +4,7 @@ from py3gpp.nrOFDMInfo import nrOFDMInfo
 
 # TODO: implement windowing
 def nrOFDMModulate(
-    carrier=None, grid=None, scs=None, initialNSlot=0, CyclicPrefix="normal", Nfft=None,
+    carrier=None, grid=None, scs=None, initialNSlot=None, CyclicPrefix="normal", Nfft=None,
     SampleRate=None, Windowing=None, CarrierFrequency=0
 ):
     info = {}
@@ -15,14 +15,20 @@ def nrOFDMModulate(
             return
         NSizeGrid = grid.shape[0] // 12
         carrier = nrCarrierConfig(NSizeGrid=NSizeGrid)
+        initialNSlot = carrier.NSlot
     else:
         NSizeGrid = carrier.NSizeGrid
+        if initialNSlot is None:
+            initialNSlot = carrier.nSlot
+        
+    if initialNSlot is None:
+        initialNSlot = 0
 
-    if scs == None:
+    if scs is None:
         scs = carrier.SubcarrierSpacing
 
-    if Nfft == None:
-        if SampleRate == None:
+    if Nfft is None:
+        if SampleRate is None:
             Nfft = nrOFDMInfo(nrb=NSizeGrid, scs=scs)["Nfft"]
             SampleRate = int(Nfft * scs * 1000)
         else:
@@ -36,7 +42,7 @@ def nrOFDMModulate(
     info["SymbolLengths"] = np.empty(0, "int")
     if len(grid.shape) == 1:
         grid = grid[..., np.newaxis]
-    nSlots = grid.shape[1]
+    nSlots = grid.shape[1] + initialNSlot
     waveform = np.empty(0, "complex")
     if CyclicPrefix == "normal":
         N_cp1 = int(((144) * 2 ** (-mu) + 16) * (SampleRate / 30720000))
@@ -45,6 +51,7 @@ def nrOFDMModulate(
         N_cp1 = int((512 * 2 ** (-mu)) * (SampleRate / 30720000))
         N_cp2 = N_cp1
 
+    sample_pos = 0
     for slot_num in range(nSlots):
         if slot_num == 0 or slot_num == 7 * 2 ** (mu):
             N_cp = N_cp1
@@ -56,6 +63,12 @@ def nrOFDMModulate(
             full_slot_grid = grid[np.abs(nFill) :, slot_num][:nFill]
         else:
             full_slot_grid = np.concatenate([np.zeros(nFill), grid[:, slot_num], np.zeros(nFill)])
+
+        # phase compensation according to TS 38.211 section 5.4
+        sample_pos += N_cp
+        full_slot_grid *= np.exp(1j * 2 * np.pi * CarrierFrequency / SampleRate * sample_pos)
+        sample_pos += Nfft
+        
         symbol_waveform = np.fft.ifft(np.fft.fftshift(full_slot_grid))
         symbol_waveform_cp = np.append(symbol_waveform[-N_cp:], symbol_waveform)
         waveform = np.append(waveform, symbol_waveform_cp)
