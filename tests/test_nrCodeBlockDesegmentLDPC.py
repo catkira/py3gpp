3ec0aebee66bc3519b02732d1cfdbcdc71f22978
Benjamin Menkuec <benjamin@menkuec.de>
2023-08-08 13:43:54 +0200

    cleanup

--- tests/test_nrCodeBlockDesegmentLDPC.py
+++ tests/test_nrCodeBlockDesegmentLDPC.py
@@ -2,18 +2,18 @@ import numpy as np
 from py3gpp.nrCodeBlockDesegmentLDPC import nrCodeBlockDesegmentLDPC
 from py3gpp.nrCRCDecode import nrCRCDecode
 
-from test_data.ldpc import *
+import test_data.ldpc
 
 def test_single_segment():
     bgn = 2
     blklen = 640 + 16 # 16 bit transport block CRC
-    rx_blk, err = nrCodeBlockDesegmentLDPC(decBits, bgn, blklen)
-    assert np.all(rx_blk == blk)
+    rx_blk, err = nrCodeBlockDesegmentLDPC(test_data.ldpc.decBits, bgn, blklen)
+    assert np.all(rx_blk == test_data.ldpc.blk)
     assert err == False
     # check transport block CRC
     out, crc = nrCRCDecode(rx_blk, '16')
     assert crc == 0
-    assert np.all(out == sib1bits)
+    assert np.all(out == test_data.ldpc.sib1bits)
 
 if __name__ == '__main__':
     test_single_segment()
--- tests/test_nrCodeBlockSegmentLDPC.py
+++ tests/test_nrCodeBlockSegmentLDPC.py
@@ -1,20 +1,28 @@
 import numpy as np
 from py3gpp.nrCodeBlockSegmentLDPC import nrCodeBlockSegmentLDPC
 
+import test_data.ldpc
+
 def array_to_int(data):
     value = 0
     for i in range(len(data)):
         value += (2 ** i) * data[i]
     return value
 
-if __name__ == '__main__':
+def test_single_segment():
+    bgn = 2
+    blk = nrCodeBlockSegmentLDPC(test_data.ldpc.decBits, bgn)
+    # test_data.ldpc.blk contains fill bytes which need to be skipped in the comparison
+    assert np.all(blk[:len(test_data.ldpc.blk), 0] == test_data.ldpc.blk)
+    
+def test_single_segment_2():
     bgn = 1
     blk = np.concatenate((np.ones(1000), np.zeros(2000), np.ones(1000))).astype(int)
     cbs1 = nrCodeBlockSegmentLDPC(blk, bgn)
     assert cbs1.shape == (4224, 1)
-    assert np.all(cbs1[0:len(blk), 0] == blk)
-    # print(f'crc = {array_to_int(cbs1[len(blk):len(blk)+24, 0]):06x}')
+    assert np.all(cbs1[0:len(blk), 0] == blk)    
 
+def test_multi_segment():
     bgn = 2
     blk = np.concatenate((np.ones(1000), np.zeros(2000), np.ones(1000))).astype(int)
     cbs2 = nrCodeBlockSegmentLDPC(blk, bgn)
@@ -24,4 +32,10 @@ if __name__ == '__main__':
     for i in range(2):
         print(f'segment {i} crc = {array_to_int(cbs2[2000:2024, i]):06x}')
     assert array_to_int(cbs2[2000:2024, 0]) == 0x271b12
-    assert array_to_int(cbs2[2000:2024, 1]) == 0x2875ad
+    assert array_to_int(cbs2[2000:2024, 1]) == 0x2875ad    
+
+if __name__ == '__main__':
+    test_single_segment()
+    test_single_segment_2()
+    test_multi_segment()
+    
