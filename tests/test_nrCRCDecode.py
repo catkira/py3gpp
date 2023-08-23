a9e30b4ccfdbecf27dc5bf2d26b78aaecec611d4
Benjamin Menkuec <benjamin@menkuec.de>
2023-08-23 16:02:33 +0200

    [fix] test_nrCRCDecode.py

--- tests/test_nrCRCDecode.py
+++ tests/test_nrCRCDecode.py
@@ -1,10 +1,14 @@
+import numpy as np
 from py3gpp.nrCRCDecode import nrCRCDecode
 
 def test_nrCRCDecode():
-    testdata = [0, 1, 0, 0, 1, 1, 1, 0, 0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 0, 1, 1, 1, 0, 1, 0, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0]
+    testdata = np.array([0, 1, 0, 0, 1, 1, 1, 0, 0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 0, 1, 1, 1, 0, 1, 0, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0])
     data, crc_result = nrCRCDecode(testdata, '24C')
     assert crc_result == 0
 
-    testdata = [1, 1, 0, 0, 1, 1, 1, 0, 0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 0, 1, 1, 1, 0, 1, 0, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0]
+    testdata = np.array([1, 1, 0, 0, 1, 1, 1, 0, 0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 0, 1, 1, 1, 0, 1, 0, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0])
     data, crc_result = nrCRCDecode(testdata, '24C')
     assert crc_result != 0
+
+if __name__ == '__main__':
+    test_nrCRCDecode()
