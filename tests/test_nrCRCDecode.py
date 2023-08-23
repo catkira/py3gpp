08419907a546b40ad4e15450a3330603d8e80f8a
Benjamin Menkuec <benjamin@menkuec.de>
2023-08-23 16:08:08 +0200

    improve test_nrCRCDecode.py

--- tests/test_nrCRCDecode.py
+++ tests/test_nrCRCDecode.py
@@ -5,10 +5,12 @@ def test_nrCRCDecode():
     testdata = np.array([0, 1, 0, 0, 1, 1, 1, 0, 0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 0, 1, 1, 1, 0, 1, 0, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0])
     data, crc_result = nrCRCDecode(testdata, '24C')
     assert crc_result == 0
+    assert np.array_equal(data[:, 0], testdata[0 : len(testdata) - 24])
 
     testdata = np.array([1, 1, 0, 0, 1, 1, 1, 0, 0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 0, 1, 1, 1, 0, 1, 0, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0])
     data, crc_result = nrCRCDecode(testdata, '24C')
     assert crc_result != 0
+    assert np.array_equal(data[:, 0], testdata[0 : len(testdata) - 24])
 
 if __name__ == '__main__':
     test_nrCRCDecode()
