bc27312246661bb1d4450f21b7bcfd69042744dd
Benjamin Menkuec <benjamin@menkuec.de>
2023-08-23 13:59:49 +0200

    add matlab test for nrCodeBlockDesegmentLDPC()

---
+++ tests/matlab/test_nrCodeBlockDesegmentLDPC.py
@@ -0,0 +1,50 @@
+import matlab.engine
+import itertools
+import numpy as np
+import pytest
+
+from py3gpp.nrCodeBlockDesegmentLDPC import nrCodeBlockDesegmentLDPC
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/..")
+import test_data.ldpc
+
+def run_nrCodeBlockDesegmentLDPC_fixed_data(eng):
+    bgn = 2
+    blklen = 640 + 16 # 16 bit transport block CRC
+    ref_data = eng.nrCodeBlockDesegmentLDPC(eng.transpose(eng.double(test_data.ldpc.decBits)), eng.double(bgn), eng.double(blklen))
+    ref_data = np.array(list(itertools.chain(*ref_data)))
+
+    data, _ = nrCodeBlockDesegmentLDPC(test_data.ldpc.decBits, bgn, blklen)
+
+    assert (ref_data == data).all()
+
+def run_nrCodeBlockDesegmentLDPC(blklen, eng):
+    bgn = 2
+    in_blk = np.random.randint(2, size = blklen)  # this is a transport block
+    cbs = eng.nrCodeBlockSegmentLDPC(eng.transpose(eng.double(in_blk)), eng.double(bgn))  # outputs code block segments
+    cbs = np.asarray(cbs)
+    ref_blk = eng.nrCodeBlockDesegmentLDPC(eng.double(cbs), eng.double(bgn), eng.double(blklen))
+    ref_blk = np.ravel(np.asarray(ref_blk).astype(int))
+    assert np.array_equal(ref_blk, in_blk)
+
+    out_blk, _ = nrCodeBlockDesegmentLDPC(cbs, bgn, blklen)
+    assert np.array_equal(ref_blk, out_blk)
+
+@pytest.fixture(scope='session')
+def eng():
+    eng = matlab.engine.connect_matlab()
+    yield eng
+    eng.quit()
+
+def test_nrCodeBlockDesegmentLDPC_fixed_data(eng):
+    run_nrCodeBlockDesegmentLDPC_fixed_data(eng)
+
+@pytest.mark.parametrize('blklen', [4000, 4001, 10000])
+def test_nrCodeBlockDesegmentLDPC(blklen, eng):
+    run_nrCodeBlockDesegmentLDPC(blklen, eng)
+
+if __name__ == '__main__':
+    _eng = matlab.engine.connect_matlab()
+    run_nrCodeBlockDesegmentLDPC(4000, _eng)
+    _eng.quit()
