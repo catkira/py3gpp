c37125ca069be35c991afdb65e73649a4b3a6138
Zulfadli Zainal <50709439+zulfadlizainal@users.noreply.github.com>
2023-09-05 15:18:17 +0900

    mod nrPathLossConfig
    convert def to class to make function property call similar as matlab docs.

--- py3gpp/configs/nrPathLossConfig.py
+++ py3gpp/configs/nrPathLossConfig.py
@@ -2,36 +2,47 @@
 
 import numpy as np
 
-def nrPathLossConfig(
-    Scenario='UMa',
-    BuildingHeight=5,
-    StreetWidth=20,
-    EnvironmentHeight=1,
-    OptionalModel=0
-):
-    # Check dependencies
-    if Scenario == 'RMa':
-        BuildingHeight = max(5, min(50, BuildingHeight))
-        StreetWidth = max(5, min(50, StreetWidth))
-    elif Scenario in ['UMa', 'UMi']:
-        if isinstance(EnvironmentHeight, (int, float)):
-            EnvironmentHeight = max(0, EnvironmentHeight)
-        elif isinstance(EnvironmentHeight, (list, np.ndarray)):
-            EnvironmentHeight = np.maximum(0, EnvironmentHeight)
-    else:
-        raise ValueError(
-            "Invalid Scenario. Supported values: 'UMa', 'UMi', 'RMa'")
 
-    OptionalModel = bool(OptionalModel)
+class nrPathLossConfig:
+    def __init__(
+        self,
+        Scenario='UMa',
+        BuildingHeight=5,
+        StreetWidth=20,
+        EnvironmentHeight=1,
+        OptionalModel=0
+    ):
+        # Check dependencies
+        if Scenario == 'UMa':
+            BuildingHeight = max(5, min(50, BuildingHeight))
+            StreetWidth = max(5, min(50, StreetWidth))
+        elif Scenario in ['UMa', 'UMi']:
+            if isinstance(EnvironmentHeight, (int, float)):
+                EnvironmentHeight = max(0, EnvironmentHeight)
+            elif isinstance(EnvironmentHeight, (list, np.ndarray)):
+                EnvironmentHeight = np.maximum(0, EnvironmentHeight)
+        else:
+            raise ValueError(
+                "Invalid Scenario. Supported values: 'UMa', 'UMi', 'RMa'")
 
-    #TODO Indoor path loss model related configuration  - currently not defined in MATLAB docs
+        OptionalModel = bool(OptionalModel)
 
-    # Return the configured parameters
-    return {
-        'Scenario': Scenario,
-        'BuildingHeight': BuildingHeight,
-        'StreetWidth': StreetWidth,
-        'EnvironmentHeight': EnvironmentHeight,
-        'OptionalModel': OptionalModel
-    }
+        # TODO Indoor path loss model related configuration  - currently not defined in MATLAB docs
 
+        # Set the attributes
+        self.Scenario = Scenario
+        self.BuildingHeight = BuildingHeight
+        self.StreetWidth = StreetWidth
+        self.EnvironmentHeight = EnvironmentHeight
+        self.OptionalModel = OptionalModel
+
+
+# Create an instance of NrPathLossConfig
+pathlossconf = nrPathLossConfig()
+
+# Example usage
+# print("Scenario:", pathlossconf.Scenario)
+# print("BuildingHeight:", pathlossconf.BuildingHeight)
+# print("StreetWidth:", pathlossconf.StreetWidth)
+# print("EnvironmentHeight:", pathlossconf.EnvironmentHeight)
+# print("OptionalModel:", pathlossconf.OptionalModel)
