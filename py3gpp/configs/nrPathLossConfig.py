b31ff14c4b640958f48fb6ea6fc98e7c2830d8c8
Zulfadli Zainal <50709439+zulfadlizainal@users.noreply.github.com>
2023-09-07 14:15:43 +0900

    fix issue #56 nrPathLossConfig
    save property as an object to allow similar object call as matlab.

--- py3gpp/configs/nrPathLossConfig.py
+++ py3gpp/configs/nrPathLossConfig.py
@@ -1,37 +1,27 @@
 # Ref: https://www.mathworks.com/help/5g/ref/nrpathlossconfig.html
 
-import numpy as np
+class nrPathLossConfig:
+    def __init__(
+        self,
+        Scenario='UMa',
+        BuildingHeight=5,
+        StreetWidth=20,
+        EnvironmentHeight=1,
+        OptionalModel=0
+    ):
+        
+        # Set the attributes
+        self.Scenario = Scenario
+        self.BuildingHeight = BuildingHeight
+        self.StreetWidth = StreetWidth
+        self.EnvironmentHeight = EnvironmentHeight
+        self.OptionalModel = OptionalModel
 
-def nrPathLossConfig(
-    Scenario='UMa',
-    BuildingHeight=5,
-    StreetWidth=20,
-    EnvironmentHeight=1,
-    OptionalModel=0
-):
-    # Check dependencies
-    if Scenario == 'RMa':
-        BuildingHeight = max(5, min(50, BuildingHeight))
-        StreetWidth = max(5, min(50, StreetWidth))
-    elif Scenario in ['UMa', 'UMi']:
-        if isinstance(EnvironmentHeight, (int, float)):
-            EnvironmentHeight = max(0, EnvironmentHeight)
-        elif isinstance(EnvironmentHeight, (list, np.ndarray)):
-            EnvironmentHeight = np.maximum(0, EnvironmentHeight)
-    else:
-        raise ValueError(
-            "Invalid Scenario. Supported values: 'UMa', 'UMi', 'RMa'")
 
-    OptionalModel = bool(OptionalModel)
-
-    #TODO Indoor path loss model related configuration  - currently not defined in MATLAB docs
-
-    # Return the configured parameters
-    return {
-        'Scenario': Scenario,
-        'BuildingHeight': BuildingHeight,
-        'StreetWidth': StreetWidth,
-        'EnvironmentHeight': EnvironmentHeight,
-        'OptionalModel': OptionalModel
-    }
-
+# # Example usage
+# pathlossconf = nrPathLossConfig()
+# print(f'Scenario: ', pathlossconf.Scenario)
+# print(f'BuildingHeight: ', pathlossconf.BuildingHeight)
+# print(f'StreetWidth: ', pathlossconf.StreetWidth)
+# print(f'EnvironmentHeight: ', pathlossconf.EnvironmentHeight)
+# print(f'OptionalModel: ', pathlossconf.OptionalModel)
