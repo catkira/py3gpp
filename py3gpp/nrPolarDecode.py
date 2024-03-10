0ae28c00def50e94ebb4e6d14bfedf29c073d991
Benjamin Menküc <benjamin@menkuec.de>
2024-03-10 14:19:07 +0100

    add tests with iil=True

--- py3gpp/nrPolarDecode.py
+++ py3gpp/nrPolarDecode.py
@@ -1,27 +1,6 @@
 import numpy as np
 from .helper import generate_5g_ranking
-
-def interleave(K):
-    # 38.212 Table 5.3.1.1-1
-    # fmt: off
-    p_IL_max_table = [0, 2, 4, 7, 9, 14, 19, 20, 24, 25, 26, 28, 31, 34, 42, 45, 49, 50, 51, 53, 54, 56, 58, 59, 61,
-                      62, 65, 66, 67, 69, 70, 71, 72, 76, 77, 81, 82, 83, 87, 88, 89, 91, 93,95, 98, 101, 104, 106,
-                      108, 110, 111, 113, 115, 118, 119, 120, 122, 123, 126, 127, 129, 132, 134, 138, 139, 140, 1, 3,
-                      5, 8, 10, 15, 21, 27, 29, 32, 35, 43, 46, 52, 55, 57, 60, 63, 68, 73, 78, 84, 90, 92, 94, 96,
-                      99, 102, 105, 107, 109, 112, 114, 116, 121, 124, 128, 130, 133, 135, 141, 6, 11, 16, 22, 30, 33,
-                      36, 44, 47, 64, 74, 79, 85, 97, 100, 103, 117, 125, 131, 136, 142, 12, 17, 23, 37, 48, 75, 80,
-                      86, 137, 143, 13, 18, 38, 144, 39, 145, 40, 146, 41, 147, 148, 149, 150, 151, 152, 153, 154, 155,
-                      156, 157, 158, 159, 160, 161, 162, 163]
-    # fmt: on
-    k = 0
-    K_IL_max = 164
-    p = np.empty(K, "int")
-    for p_IL_max in p_IL_max_table:
-        if p_IL_max >= (K_IL_max - K):
-            p[k] = p_IL_max - (K_IL_max - K)
-            k += 1
-    return p
-
+from .helper import polar_precode_interleave
 
 # very simple successive cancellation decoder
 def Polar_SC_decoder(N, frozen_pos, r):
@@ -89,7 +68,7 @@ def nrPolarDecode(rec, K, E, L, padCRC=False, nmax=9, iil=True, CRClen=24):
 
     if iil:
         # deinterleave
-        p_IL = interleave(K)
+        p_IL = polar_precode_interleave(K)
         decoded2 = np.empty(decoded.shape, "int")
         np.put(decoded2, p_IL, decoded)
     else:
